2927e3561a4dc4b6e93ecde4a83e8b95e9fdbcaf
Caroline Richards <caroline.piper.richards@gmail.com>
2025-12-13 19:58:23 -0500

    Update About section to indicate website now outdated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,14 +14,14 @@ function About() {
                 <img src = {piper} className = 'flex-child' alt = ""/>
                 <div className = 'flex-child'>
                     <h3>Hi</h3>
-                    <p>Hey! My name is Caroline Richards. I'm a 17 year old student passionate about books, nature, and computers. Originally I wanted to be a writer but then was accidentally placed in a coding class and discovered coding and hacking too. Now I do some of both, and compete in CTFs and design games outside of school.</p>
-                    <h4>Extracirriculars</h4>
+                    <p>This is my old high school website! It hosts many of the projects I worked on my senior year. Now I'm a college student at Rochester Institute of Technology. A lot of the stuff on this website is a bit dated now, so if you're interested in the more recent stuff I've been up to visit my <a href = "www.linkedin.com/in/caroline-p-richards">LinkedIn</a> or my <a href = "https://github.com/nixandruin">GitHub</a>.</p>
+                    <h4>(Highschool) Extracirriculars</h4>
                     <ul>
                         <li>LB Varisity Rifle (Co-Captain)</li>
                         <li>Latin Club and Certamen (Consul)</li>
                         <li>CyberPatriot (Co-Founder)</li>
                     </ul>
-                    <h4>Work Experience</h4>
+                    <h4> (Highschool) Work Experience</h4>
                     <ul>
                         <li>NOVA Children's Science Center Spring Inter (spring 2023)</li>
                         <li>Camp Seafarer Archery CILT (summer 2023)</li>
